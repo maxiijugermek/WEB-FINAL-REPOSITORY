f53ca9695714b52dc40b9028d8eb8c2c27bbf773
maxiijugermek <maxijugermek@gmail.com>
2016-05-20 20:01:32 +0600

    Add files via upload

---
+++ phpsite/blocks/footerr.php
@@ -0,0 +1,15 @@
+﻿
+<div class="footerr">
+	<div class="footer_menu">
+	<ul>
+    	<li><a href="http://twitter.com"><img src="twitter.png" width="70px" height="60px"></a></li>
+        <li><a href="http://gmail.com"><img src="gmail.png" width="70px" height="60px"></a></li>
+        <li><a href="http://facebook.com"><img src="facebook.jpg" width="70px" height="60px"></a></li>
+        <li><a href="http://instagram.com"><img src="instagram.jpg" width="70px" height="60px"></a></li>
+        <li><a href="http://vk.com"><img src="vk.jpeg" width="70px" height="60px"></a></li>
+    </ul>
+
+	    
+    
+    </div>
+</div>
---
+++ phpsite/blocks/header.php
@@ -0,0 +1,16 @@
+﻿<!--<tr>
+      <td><img src="image/header.jpg" width="690" height="100" alt=""/></td>
+</tr>-->
+<div class="header_one">
+	<div class="left_menu">
+    	<ul>
+        	<a href="index.php"><li>Main</li></a>
+            <a href="articles.php"><li>Articles</li></a>
+            <a href="lessons.php"><li>Lessons</li></a>
+            <a href="contacts.php"><li>About Us</li></a>
+            <a href="#" class="user_nam"><li><?php echo $myrow['username']; ?></li></a>
+            <a href="logout.php" class="loggin_out"><li >Logout</li></a>
+        </ul>
+    </div>
+    
+</div>
